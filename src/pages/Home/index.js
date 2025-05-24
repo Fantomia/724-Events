69f4fdf5b11bbf801d349317ad753b59dc3cd21c
Fantomia <jonathan.andri@gmail.com>
2025-05-24 17:38:34 +0200

    Correction des fautes de frappe, ajout des ID de navigation, correction des erreurs pour la dernière prestation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -29,7 +29,7 @@ const Page = () => {
       <section className="SliderContainer">
         <Slider />
       </section>
-      <section className="ServicesContainer">
+      <section className="ServicesContainer" id="nos-services">
         <h2 className="Title">Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
@@ -58,13 +58,13 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
+      <section className="EventsContainer" id="nos-realisations">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
+      <section className="PeoplesContainer" id="notre-equipe">
         <h2 className="Title">Notre équipe</h2>
-        <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
+        <p>Une équipe d’experts dédiés à l’organisation de vos événements</p>
         <div className="ListContainer">
           <PeopleCard
             imageSrc="/images/stephanie-liverani-Zz5LQe-VSMY-unsplash.png"
@@ -121,6 +121,7 @@ const Page = () => {
       </div>
     </main>
     <footer className="row">
+      {last && (
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
         <Modal Content={<ModalEvent event={last} />}>
@@ -136,6 +137,7 @@ const Page = () => {
             )}
         </Modal>
       </div>
+      )}
       <div className="col contact">
         <h3>Contactez-nous</h3>
         <address>45 avenue de la République, 75000 Paris</address>
